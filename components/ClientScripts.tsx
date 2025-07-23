715d1d407dfcb3355781a0d16ef9aef31d60cf43
wilsonaustin10 <107770258+wilsonaustin10@users.noreply.github.com>
2025-07-22 21:05:18 -0500

    Add src/components/ClientScripts.tsx from template

---
+++ components/ClientScripts.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function ClientScripts() {
+  return (
+    <>
+      <Script
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+        strategy="beforeInteractive"
+        onLoad={() => {
+          console.log('Google Maps script loaded');
+        }}
+        onError={(e) => {
+          console.error('Error loading Google Maps script:', e);
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17041108639"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17041108639');
+          `,
+        }}
+      />
+    </>
+  );
+}
